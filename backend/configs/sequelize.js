f2427fa1dec0abeeb5d22e9c1f9e09d5a866fb43
vedanivas <veda.nivas@students.iiit.ac.in>
2024-03-19 01:54:10 +0530

    add error logging

--- backend/configs/sequelize.js
+++ backend/configs/sequelize.js
@@ -21,7 +21,8 @@ try {
   await sequelize.authenticate();
   console.log('Connection has been established successfully.');
 } catch (error) {
-  console.error('Unable to connect to the database:', error);
+  console.error('Unable to connect to the database')
+  throw error
 }
 
 export default sequelize;
